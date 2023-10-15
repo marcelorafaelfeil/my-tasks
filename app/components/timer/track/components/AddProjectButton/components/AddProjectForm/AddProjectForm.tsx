2f5f07239f046f8cd32738e1ed208e41ad7a5262
Marcelo Feil <marcelo.rafael.feil@gmail.com>
2023-10-15 23:50:45 +0100

    Changed eol and configured inputs

--- app/components/timer/track/components/AddProjectButton/components/AddProjectForm/AddProjectForm.tsx
+++ app/components/timer/track/components/AddProjectButton/components/AddProjectForm/AddProjectForm.tsx
@@ -1,8 +1,13 @@
 import { Input, Switch } from '@nextui-org/react';
+import { useState } from 'react';
 import { AiOutlineFolder } from 'react-icons/ai';
 import { BiDollar } from 'react-icons/bi';
 
 export const AddProjectForm = () => {
+  const [projectName, setProjectName] = useState('');
+  const [billing, setBilling] = useState(false);
+  const [hourPrice, setHourPrice] = useState('0');
+
   const handleSubmit = () => {
     console.log('submitted');
   };
@@ -18,20 +23,28 @@ export const AddProjectForm = () => {
         type="text"
         aria-label="Project's name"
         data-1p-ignore
-      />
-      <Input
-        autoFocus
-        endContent={<BiDollar />}
-        className="no-spin"
-        label="Hour price"
-        variant="bordered"
-        type="number"
-        aria-label="Project's name"
-        data-1p-ignore
+        value={projectName}
+        onValueChange={setProjectName}
       />
       <div>
-        <Switch defaultSelected>Billing</Switch>
+        <Switch isSelected={billing} onValueChange={setBilling} defaultSelected>
+          Billing
+        </Switch>
       </div>
+      {billing && (
+        <Input
+          autoFocus
+          endContent={<BiDollar />}
+          className="no-spin"
+          label="Hour price"
+          variant="bordered"
+          type="number"
+          aria-label="Project's name"
+          data-1p-ignore
+          onValueChange={(v) => setHourPrice(v)}
+          value={hourPrice}
+        />
+      )}
     </form>
   );
 };
