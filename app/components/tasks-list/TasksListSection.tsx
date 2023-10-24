e0b7c4a482577fb39e7258c407da070225a2b093
Marcelo Feil <marcelo.rafael.feil@gmail.com>
2023-10-24 01:25:07 +0100

    Mapped tasks to be divided in groups and printed on the screen

---
+++ app/components/tasks-list/TasksListSection.tsx
@@ -0,0 +1,5 @@
+import { TaskList } from './components/TaskList/TaskList';
+
+export const TasksListSection = () => {
+  return <TaskList />;
+};
--- app/services/TaskService.ts
+++ app/services/TaskService.ts
@@ -1,4 +1,6 @@
+import { format, parse } from 'date-fns';
 import { IndexableType } from 'dexie';
+import { GroupedTasks } from '../components/tasks-list/components/TaskList/types/GroupedTasks';
 import { Project } from '../components/timer/track/components/context/types/Project';
 import { Task } from '../components/timer/track/components/context/types/Task';
 import { TaskStatus } from '../components/timer/track/components/context/types/TaskStatus';
@@ -48,4 +50,44 @@ export const stopCurrentTask = async (): Promise<number> => {
 
 export const getOngoingTask = async (): Promise<Task | undefined> => {
   return await db.tasks.where('status').equals(TaskStatus.ONGOING).first();
+};
+
+export const getAllFinishedTasks = async (): Promise<GroupedTasks[]> => {
+  const allFinishedTasks: Task[] = await db.tasks
+    .where('status')
+    .equals(TaskStatus.FINISHED)
+    .toArray();
+  // const tasksByDay: Task[] = [];
+
+  const reducedItems = allFinishedTasks.reduce(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (accumulator: any, item: Task) => {
+      if (item.startTime) {
+        const key: string = format(item.startTime, 'yyyyMMdd');
+
+        return {
+          ...accumulator,
+          [key]: [...(accumulator[key]! ?? []), item],
+        };
+      }
+      return {} as Task;
+    },
+    {},
+  );
+
+  const groupedTasks: GroupedTasks[] = [];
+
+  Object.keys(reducedItems).forEach((key: string, index: number) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const item = reducedItems[key];
+    groupedTasks.push({
+      key: index,
+      date: parse(key, 'yyyyMMdd', new Date()),
+      items: item.map((task: Task) => ({
+        ...task,
+      })),
+    } as GroupedTasks);
+  });
+
+  return groupedTasks;
 };
